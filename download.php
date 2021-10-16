a61818d7021ce41ebb38067a3a3cad947bb68daf
Tomáš Lokša <loksa@kros.sk>
2021-10-16 15:41:53 +0200

    first commit

---
+++ download.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="sk">
+<head>
+  <?php include("shared/head.html"); ?>
+</head>
+
+<body>
+	<header>
+    <?php include("shared/header.html"); ?>
+	</header>
+
+  <div class="content">
+    <p>
+      Dochádzkového klienta pre zamestnancov je možné stiahnuť tu: 
+      <br><br>
+      <a class="btn w3-round-large w3-xlarge btn-primary" href="https://powacraft.sk/Dochadzka/Verzia/publish.htm">Stiahnuť</a>
+      <br><br>
+      Správcu dochádzky je možné stiahnuť len po zakúpení. V prípade, že máte záujem o kompletné riešenie, prosím, kontaktujte ma.
+    </p>
+
+  </div>
+
+	<footer>
+    <?php include("shared/footer.html"); ?>
+	</footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="sk">
+<head>
+  <?php include("shared/head.html"); ?>
+</head>
+
+<body>
+	<header>
+    <?php include("shared/header.html"); ?>
+	</header>
+
+  <div class="content">
+    <p>Jednoduchý dochádzkový systém, ktorý zapisuje dochádzku na základe stavu počítača.</p>
+    <img class="img-preview" src="img/dochadzka.png">
+    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sem elit, ornare id consequat et, tristique sit amet turpis. Nunc nec vestibulum augue. 
+    Donec sodales lorem ut sem maximus, sit amet fermentum ligula aliquet. Fusce sit amet luctus diam. Suspendisse consectetur diam vel ipsum posuere pharetra 
+    eu vitae tellus. Donec in facilisis velit. Quisque quis gravida diam. Proin vitae erat quis neque facilisis posuere. Phasellus quis orci nisi. Quisque in 
+    sem non libero volutpat vehicula eu et nisi. Fusce vestibulum bibendum cursus. Aliquam nec molestie massa. Sed maximus sodales turpis, a suscipit augue 
+    fermentum id. Mauris hendrerit enim ut magna tincidunt porta. </p>
+  </div>
+
+	<footer>
+    <?php include("shared/footer.html"); ?>
+	</footer>
+</body>
+</html>
